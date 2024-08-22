54cb117e2274b50ba103f6e4825e2e90e80af959
Wei Su <61648647+gnswork@users.noreply.github.com>
2024-08-22 10:01:29 +0800

    Add infinite scroll by react-swr-infinite-scroll.

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,8 +1,21 @@
+import GithubList from '@/app/ui/customers/github';
+import { lusitana } from '@/app/ui/fonts';
 import { Metadata } from 'next';
+
 export const metadata: Metadata = {
   title: 'Customers',
 };
 
 export default function Page() {
-  return <p>Customers Page</p>;
+  return (
+    <div className="w-full">
+      <div className="flex w-full items-center justify-between">
+        <h1 className={`${lusitana.className} text-2xl`}>Customers</h1>
+      </div>
+      {/* <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
+        <Table query={query} currentPage={currentPage} />
+      </Suspense> */}
+      <GithubList />
+    </div>
+  );
 }
---
+++ app/ui/customers/github.tsx
@@ -0,0 +1,66 @@
+'use client';
+
+import React from 'react';
+// import { useSWRInfinite } from 'swr';
+import useSWRInfinite from 'swr/infinite';
+
+// import LoadingIndicator from './LoadingIndicator';
+// import InfiniteScroll from '.';
+import InfiniteScroll from './infinitescroll';
+
+// export default {
+//   title: 'InfiniteScroll/GitHub',
+//   component: InfiniteScroll,
+// };
+
+export default function GithubList() {
+  // export const GitHub: React.FC = () => {
+  const PAGE_SIZE = 5;
+
+  const swr = useSWRInfinite(
+    (index) =>
+      `https://api.github.com/repos/reactjs/react-a11y/issues?per_page=${PAGE_SIZE}&page=${
+        index + 1
+      }`,
+    {
+      fetcher: async (key) => fetch(key).then((res) => res.json()),
+    },
+  );
+
+  return (
+    <InfiniteScroll
+      swr={swr}
+      // loadingIndicator={<LoadingIndicator style={{ margin: '30px auto' }} />}
+      endingIndicator={
+        <div style={{ margin: '30px auto', textAlign: 'center' }}>
+          No more issues! 🎉
+        </div>
+      }
+      isReachingEnd={(swr) =>
+        swr.data?.[0]?.length === 0 ||
+        swr.data?.[swr.data?.length - 1]?.length < PAGE_SIZE
+      }
+    >
+      {(response) =>
+        response?.map((issue) => (
+          <div
+            key={issue.id}
+            style={{
+              padding: '20px',
+              borderRadius: '8px',
+              border: 'solid #ccc 1px',
+              margin: '20px auto',
+              maxWidth: '400px',
+            }}
+          >
+            <div style={{ fontWeight: 700 }}>{issue.title}</div>
+            <div style={{ color: '#aaa', marginTop: '8px' }}>
+              {issue.user.login} • {new Date(issue.created_at).toDateString()}
+            </div>
+          </div>
+        ))
+      }
+    </InfiniteScroll>
+  );
+}
+// GitHub.storyName = 'GitHub';
