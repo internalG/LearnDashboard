36a0063ead9514ad783be00434185fc2cc1429c3
internalG <834419+internalG@users.noreply.github.com>
2024-08-23 16:43:59 +0800

    Add icon button.

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,9 @@ import styles from '@/app/ui/home.module.css';
 import { lusitana } from '@/app/ui/fonts';
 import Image from 'next/image';
 
+import Button from '@mui/material/Button';
+import LoginIcon from '@mui/icons-material/Login';
+
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
@@ -26,12 +29,9 @@ export default function Page() {
             </a>
             , brought to you by Vercel.
           </p>
-          <Link
-            href="/login"
-            className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
-          >
-            <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
-          </Link>
+          <Button href="/login" variant="contained" endIcon={<LoginIcon />}>
+            Log in
+          </Button>
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           <Image
