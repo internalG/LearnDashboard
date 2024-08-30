fc5322bc12ad1b9ce10a770be9a9d12c0729e612
internalG <834419+internalG@users.noreply.github.com>
2024-08-30 15:11:16 +0800

    Test build.

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,6 @@ import { AppRouterCacheProvider } from '@mui/material-nextjs/v13-appRouter';
 import { ThemeProvider } from '@mui/material/styles';
 import theme from '@/app/ui/theme';
 
-export const runtime = 'edge';
-
 import { Metadata } from 'next';
 
 export const metadata: Metadata = {
